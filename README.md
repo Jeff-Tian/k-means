2e933eb248bad60750b452221129421f7fa2f6ab
田杰 <jefftian@hcdlearning.com>
2016-05-28 19:12:09 +0800

    delete unnecessary files.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # k-means
 Online demo for k-means clustering process: http://k-means.jijiyy.me.
+
+# Run on local machine
+```
+npm install
+bower install
+gulp
+```
--- gulpfile.js
+++ gulpfile.js
@@ -43,60 +43,10 @@ gulp.task('replace', function (done) {
     var replace = require('gulp-replace');
 
     gulp.src(['public/semantic/dist/semantic.min.css'])
-        //.pipe(replace(/https:\/\/fonts.googleapis.com\/css/g, 'http://fonts.useso.com/css'))
+    //.pipe(replace(/https:\/\/fonts.googleapis.com\/css/g, 'http://fonts.useso.com/css'))
         .pipe(replace(/https:\/\/fonts.googleapis.com\/css/g, ''))
         .pipe(gulp.dest('public/semantic/dist/'))
     ;
-
-    done();
-});
-
-gulp.task('saveGraph', function (done) {
-    var GraphWorld = require('./cube/graph.js');
-    var CubeWorld = require('./cube/cube.js');
-    var Iterator = require('./cube/iterator.js');
-
-    var cube = CubeWorld.Cube.getPristineCube();
-    var iter = new Iterator.CubeIterator();
-    var g = iter.traverse(cube);
-
-    var fs = require('fs');
-    fs.writeFileSync('./cube.csv', g.serializeToCSV());
-
-    done();
-});
-
-gulp.task('bfl', function (done) {
-    var CubeWorld = require('./cube/cube.js');
-    var Iterator = require('./cube/iterator.js');
-    var iter = new Iterator.CubeIterator();
-    iter.breadthFirstTraverseLite(CubeWorld.Cube.getPristineCube());
-
-    done();
-});
-
-gulp.task('fast', function (done) {
-    var CubeLite = require('./cube/cube-lite');
-    var Iterator = require('./cube/iterator');
-    var iter = new Iterator.CubeIterator();
-    iter.breadthFirstTraverseQuick(CubeLite.getPristineCube());
-
-    done();
-});
-
-gulp.task('saveMemory', function (done) {
-    var CubeLite = require('./cube/cube-lite');
-    var Iterator = require('./cube/iterator');
-    var iter = new Iterator.CubeIterator();
-    iter.breadthFirstTraverseFile(CubeLite.getPristineCube());
-
-    done();
-});
-
-gulp.task('restricted-search', function (done) {
-    var CubeLite = require('./cube/cube-lite');
-    var Iterator = require('./cube/iterator');
-    Iterator.CubeIterator.iterateAndSaveFile(CubeLite.getPristineCube(), 'restricted-cube.csv');
 
     done();
 });
